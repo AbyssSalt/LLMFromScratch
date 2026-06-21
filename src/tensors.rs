1773accbcb463e3607d6c44c573ba29d5c089ae4
Johnathan-Lavoie <johnathan.lavoie@ontariotechu.net>
2026-06-21 15:21:05 -0400

    added element-wise operations and added error handling to the functions

--- src/tensors.rs
+++ src/tensors.rs
@@ -1,14 +1,9 @@
-
-
 struct Tensor {
     data: Vec<f32>,
     // x, y, z, w
     sizes: [usize;4],
     strides: [usize;4],
 }
-
-
-
 
 fn mult_matrices(a: &Tensor, b: &Tensor, out: &mut Tensor, a_offset: usize, b_offset: usize, out_offset: usize) {
     // AxB results in an Ay by Bx matrix
@@ -34,11 +29,14 @@ fn mult_matrices(a: &Tensor, b: &Tensor, out: &mut Tensor, a_offset: usize, b_of
     }
 }
 
-fn mult_tensors(a: &Tensor, b: &Tensor) -> Tensor {
+fn mult_tensors(a: &Tensor, b: &Tensor) -> Option<Tensor> {
+    if a.sizes[0] != b.sizes[1] || a.sizes[2] != b.sizes[2] || a.sizes[3] != b.sizes[3] {
+        return None
+    }
+
     let out_sizes: [usize; 4] = [b.sizes[0], a.sizes[1], a.sizes[2], a.sizes[3]];
     let out_strides: [usize; 4] = [1, out_sizes[0], out_sizes[1] * out_sizes[0], out_sizes[2] * out_sizes[1] * out_sizes[0]];
     let mut out: Tensor = Tensor { data: vec![0.0; out_strides[3] * out_sizes[3]], sizes: (out_sizes), strides: (out_strides) };
-    
 
     for d3 in 0..a.sizes[2] {
         for d4 in 0..a.sizes[3] {
@@ -49,7 +47,34 @@ fn mult_tensors(a: &Tensor, b: &Tensor) -> Tensor {
         }
     }
     
+    return Some(out);
+}
+
+fn add_tensors(a: &Tensor, b: &Tensor) -> Option<Tensor> {
+    if a.sizes != b.sizes {
+        return None
+    }
+    let mut out: Tensor = Tensor { data: (vec![0.0; a.data.len()]), sizes: (a.sizes), strides: (a.strides) };
+    for i in 0..a.data.len(){
+        out.data[i] = a.data[i] + b.data[i];
+    }
+    return Some(out);
+}
+
+fn scale_tensor(a: &Tensor, b: f32) -> Tensor {
+    let mut out: Tensor = Tensor { data: (vec![0.0; a.data.len()]), sizes: (a.sizes), strides: (a.strides) };
+    for i in 0..a.data.len(){
+        out.data[i] = a.data[i] * b;
+    }
     return out;
+}
+
+fn activation_func(a: &Tensor) -> Tensor {
+    let mut out: Tensor = Tensor { data: (vec![0.0; a.data.len()]), sizes: (a.sizes), strides: (a.strides) };
+    for i in 0..a.data.len(){
+        out.data[i] = (a.data[i]).max(0.0);
+    }
+    return out
 }
 
 pub fn main() {
